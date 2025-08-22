b731c902e4072683758a77afb6f6a2fdbbe569b6
UmeshGardas <umeshgardas123@gmail.com>
2025-08-22 10:52:35 +0530

    created boostrap

---
+++ script.js
@@ -0,0 +1,159 @@
+// JS
+
+// Js is a scripting langauage for web applications
+
+// Benifits
+//  - JS provides end-to-end development frontend(JS, React etc) and backend(Nodejs, expressJs etc)
+// JS is lightweight language
+
+// var name
+// var Name
+
+// What we can do using JS
+//  - Manupulating HTML pages
+//  - Validation
+//  - Data handle
+//  - Event handling
+//  - Backend data loading
+//  - Server management
+
+// Execuation Context
+// 1.Creation Phase           2. Execuation Phase
+
+// Funcational Context
+// 1.Creation Phase           2. Execuation Phase
+
+// var
+// it is  Global scope
+// it can be reassign the value and it can redeclare the variable
+// var e = "Maxxzoom";
+// var e = "zoom";
+// var f = "Limited";
+// function add() {
+//   var g = e + f;
+//   console.log(g);
+// }
+
+// add();
+
+// let
+// it is block scope
+// it can be reassign the value but we cannot redeclare the same variable
+// let i = 5;
+// // i = 6;
+// let j = 34;
+// function add2() {
+//   let k = i + j;
+//   console.log(k);
+// }
+// console.log(k);
+
+// var a = 34;
+// let b = 78;
+
+// const
+// it is block scope
+// it cannot be reassign and it cannot be redeclare
+// const PI = 3.14;
+// console.log(PI);
+
+// function add() {
+//   let c = 54;
+//   console.log(a + b);
+// }
+
+// add();
+
+// JS operators
+
+// Assignment operator (=)
+// let d = 30;
+// Variable variablename =  value;
+
+// Addition operator
+// let f = 5;
+// let g = 7;
+// let h = f + g;
+
+// Subtraction
+// Multiplication
+
+// let f = 5;
+// let g = 7;
+// console.log(f * g);
+
+// Exponentiation
+// let f = 2;
+// let g = 3;
+// console.log(f ** g);
+
+// Division(/)
+// Modulus (%)
+
+// Increment
+// let i = 2;
+
+// console.log(i++);
+// console.log(i);
+
+// Decrement
+// let i = 2;
+
+// console.log(i--);
+// console.log(i);
+
+// Addition Assignment operator
+// let j=3;
+// j+=2; /* j=j+2 */
+// console.log(j);
+
+// Comparison operators
+// equal to
+// let a = 5;
+// let b = 6;
+// if (a == b) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// equal value and equal type
+// let a = 6;
+// let b = "6";
+// if (a === b) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// let a = true;
+// let b ="true";
+// if (a == b) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// Functions
+
+// Funcation are fundamental building blocks in all programing lang
+// Funcation are resuable block of code
+
+function add(a, b, c = 0) {
+  console.log(a + b + c);
+  
+}
+
+add(5, 6, 9);
+add(6, 8);
+add(6, 8);
+add(6, 8);
+add(6, 8);
+add(6, 8);
+add(6, 8);
+add(6, 8);
+add(6, 8);
+add(6, 8);
+add(6, 8);
+add(6, 8);
+add(6, 8);
