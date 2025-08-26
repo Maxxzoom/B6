b9378c5e4718c748edefa21184c7f5818b5e09b6
UmeshGardas <umeshgardas123@gmail.com>
2025-08-26 13:18:47 +0530

    Array methods added

--- script.js
+++ script.js
@@ -141,19 +141,128 @@
 
 function add(a, b, c = 0) {
   console.log(a + b + c);
-  
 }
 
-add(5, 6, 9);
-add(6, 8);
-add(6, 8);
-add(6, 8);
-add(6, 8);
-add(6, 8);
-add(6, 8);
-add(6, 8);
-add(6, 8);
-add(6, 8);
-add(6, 8);
-add(6, 8);
-add(6, 8);
+// Arrow function
+const addition = (x, y) => {
+  console.log(x, y);
+};
+
+// addition(4, 5);
+
+// add(5, 6, 9);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+// add(6, 8);
+
+// Objects
+// an object is a collection of properties
+
+const mobile = {
+  model: "iphone",
+  memory: 128,
+  screen: "6.7",
+  camera: "8 mega pixel",
+  getScreenandMemory: function () {
+    return (
+      "Screen size is" +
+      " " +
+      this.screen +
+      " " +
+      "and memory is" +
+      " " +
+      this.memory
+    );
+  },
+};
+
+// console.log(mobile.model);
+// console.log(mobile.memory);
+// console.log(mobile.getScreenandMemory());
+
+// Update/add property
+// mobile.memory = 256;
+// mobile.sdmemory = 64;
+// console.log(mobile.memory);
+// console.log(mobile.sdmemory);
+
+// delete properties
+// delete mobile.memory;
+// console.log(mobile.memory);
+
+// Array
+// This is special kind of object
+// is not fixed length
+
+// 3 ways of arr
+
+// 1
+const arr = ["first", 83, true, false, undefined, null];
+// console.log(typeof arr,"type of array");
+
+// 2
+const arr1 = [];
+arr1[0] = "umesh";
+arr1[2] = false;
+// console.log(arr1);
+
+// 3
+// const arr2=new Array();
+// arr2[2]=true;
+// console.log(arr2);
+
+// Methods in the array
+
+// length
+// console.log(arr.length);
+
+// push();
+arr1.push(3);
+// console.log(arr1);
+
+// toString()
+// console.log(arr1.toString());
+
+// pop()
+console.log(arr1);
+// console.log(arr1.pop());
+
+// shift()
+// console.log(arr1.shift());
+
+// unshift()
+// arr1.unshift("maxxzoom");
+// console.log(arr1);
+
+// splice()
+// add new element into array at any specific index
+arr1.splice(1, 2, "tata", "altroz");
+// arr1.splice(
+//   "starting index",
+//   "how many element you want to delete",
+//   " if want to add some data(optional)"
+// );
+// console.log(arr1);
+
+// concat();
+// joining 2 arrays
+console.log(arr.concat(arr1));
+console.log(arr.concat(32));
+
+
+// slice()
+// recreate any array from the existing array
+console.log(arr.slice(1,4));
+
+
+
+//Array - map,filter, find, reduce, foreach,indexOf, lastIndexOf 
